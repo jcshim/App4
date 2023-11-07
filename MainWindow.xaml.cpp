5f515cd4b57067c141f4d769dc9d858e3f503b22
shim <shim@JAECHANG>
2023-11-07 11:37:46 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+using namespace winrt::Microsoft::Graphics::Canvas::UI::Xaml;
+struct winrt::Windows::UI::Color col = winrt::Microsoft::UI::Colors::Green();
+namespace winrt::App4::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+        px = 200;
+        py = 200;
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
+
+
+void winrt::App4::implementation::MainWindow::CanvasControl_PointerMoved(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e)
+{
+    CanvasControl canvas = sender.as<CanvasControl>();
+    px = e.GetCurrentPoint(canvas).Position().X;
+    py = e.GetCurrentPoint(canvas).Position().Y;
+    canvas.Invalidate();
+}
+
+
+void winrt::App4::implementation::MainWindow::CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args)
+{
+    args.DrawingSession().DrawEllipse(px, py, 80, 60, col, 8);
+    args.DrawingSession().DrawText(L"Hello", px - 25, py - 21, winrt::Microsoft::UI::Colors::Yellow());
+}
---
+++ MainWindow.xaml.h
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "MainWindow.g.h"
+#include <winrt/Microsoft.Graphics.Canvas.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Input.h>
+#include <winrt/Microsoft.UI.Input.h>
+
+namespace winrt::App4::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+        float px, py;
+
+        void CanvasControl_PointerMoved(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        void CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args);
+    };
+}
+
+namespace winrt::App4::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
